c279f1129e5e4f26b6c8e7f9414024a3c8cd8ba8
Stefan Slaveykov <wizard.oneandonly@gmail.com>
2017-01-31 15:05:15 +0200

    Mighty

---
+++ lib/occupator.rb
@@ -0,0 +1,5 @@
+require "occupator/version"
+
+module Occupator
+  # Your code goes here...
+end
---
+++ spec/occupator_spec.rb
@@ -0,0 +1,11 @@
+require "spec_helper"
+
+RSpec.describe Occupator do
+  it "has a version number" do
+    expect(Occupator::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
