0b20c373aa698227db8edb9b03fa05d3056266d7
Stefan Slaveykov <wizard.oneandonly@gmail.com>
2017-02-03 11:10:19 +0200

    start writing some tests

--- lib/occupator.rb
+++ lib/occupator.rb
@@ -2,9 +2,8 @@ require "occupator/version"
 
 module Occupator
 
-  class << self
-    def occupy(**options)
-      call
-    end
+  def occupy(**options)
+    every = options[:every] || :day
+    call(every, at)
   end
 end
--- spec/occupator_spec.rb
+++ spec/occupator_spec.rb
@@ -1,11 +1,24 @@
 require "spec_helper"
+require 'byebug'
 
 RSpec.describe Occupator do
+  subject do
+    class EventObject
+      extend Occupator
+    end
+  end
   it "has a version number" do
     expect(Occupator::VERSION).not_to be nil
   end
 
   it "does something useful" do
     expect(false).to eq(true)
+  end
+
+
+  describe 'Event Object' do
+    it 'responds to the occupy method' do
+      expect(subject).to respond_to(:occupy)
+    end
   end
 end
